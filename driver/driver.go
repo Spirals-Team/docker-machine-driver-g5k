fdaa598e91f24f91c543bef00ee895efc4d8f7a9
gfieni <gfieni@users.noreply.github.com>
2017-03-21 13:14:30 +0100

    feat(driver): Adding auto-generated SSH key pair, moving Job/Deployment parts to a file, improving error messages.

--- driver/driver.go
+++ driver/driver.go
@@ -3,26 +3,22 @@ package driver
 import (
 	"errors"
 	"fmt"
-	"io/ioutil"
-	"os"
 
 	"github.com/Spirals-Team/docker-machine-driver-g5k/api"
 
 	"github.com/docker/machine/libmachine/drivers"
 	"github.com/docker/machine/libmachine/log"
 	"github.com/docker/machine/libmachine/mcnflag"
-	"github.com/docker/machine/libmachine/mcnutils"
+	"github.com/docker/machine/libmachine/ssh"
 	"github.com/docker/machine/libmachine/state"
 )
 
 // Driver parameters
 type Driver struct {
 	*drivers.BaseDriver
-	*api.Api
 
-	G5kJobID        int
-	G5kDeploymentID string
-
+	G5kAPI                *api.Api
+	G5kJobID              int
 	G5kUsername           string
 	G5kPassword           string
 	G5kSite               string
@@ -32,6 +28,7 @@ type Driver struct {
 	G5kImage              string
 	G5kResourceProperties string
 	G5kHostToProvision    string
+	SSHKeyPair            *ssh.KeyPair `json:"-"` // remove this field from machine config
 }
 
 // NewDriver creates and returns a new instance of the driver
@@ -47,13 +44,6 @@ func NewDriver() *Driver {
 // DriverName returns the name of the driver
 func (d *Driver) DriverName() string {
 	return "g5k"
-}
-
-func (d *Driver) getAPI() *api.Api {
-	if d.Api == nil {
-		d.Api = api.NewApi(d.G5kUsername, d.G5kPassword, d.G5kSite)
-	}
-	return d.Api
 }
 
 // GetCreateFlags add command line flags to configure the driver
@@ -85,13 +75,13 @@ func (d *Driver) GetCreateFlags() []mcnflag.Flag {
 
 		mcnflag.StringFlag{
 			Name:  "g5k-ssh-private-key",
-			Usage: "Path of your ssh private key",
-			Value: mcnutils.GetHomeDir() + "/.ssh/id_rsa",
+			Usage: "Path of your ssh private key (Only RSA keys in PEM format are supported)",
+			Value: "",
 		},
 
 		mcnflag.StringFlag{
 			Name:  "g5k-ssh-public-key",
-			Usage: "Path of your ssh public key",
+			Usage: "Path of your ssh public key (Only RSA keys in AuthorizedKey format are supported) ",
 			Value: "",
 		},
 
@@ -151,19 +141,9 @@ func (d *Driver) SetConfigFromFlags(opts drivers.DriverOptions) error {
 		return errors.New("You must give the site you want to reserve the resources on")
 	}
 
-	// check if private key exist
-	if _, err := os.Stat(d.G5kSSHPrivateKeyPath); os.IsNotExist(err) {
-		return errors.New("Your ssh private key file does not exist in : '" + d.G5kSSHPrivateKeyPath + "'")
-	}
-
-	// if the user dont specify a public key path, append .pub to the private key path
-	if d.G5kSSHPublicKeyPath == "" {
-		d.G5kSSHPublicKeyPath = d.G5kSSHPrivateKeyPath + ".pub"
-	}
-
-	// check if public key exist
-	if _, err := os.Stat(d.G5kSSHPublicKeyPath); os.IsNotExist(err) {
-		return errors.New("Your ssh public key file does not exist in : ''" + d.G5kSSHPublicKeyPath + "'")
+	// use of provided SSH key
+	if d.G5kSSHPrivateKeyPath != "" {
+		return fmt.Errorf("Use of existing SSH keys disabled for now")
 	}
 
 	return nil
@@ -211,9 +191,8 @@ func (d *Driver) GetURL() (string, error) {
 
 // GetState returns the state of the node
 func (d *Driver) GetState() (state.State, error) {
-	client := d.getAPI()
-
-	status, err := client.GetJobState(d.G5kJobID)
+	// get job state from API
+	status, err := d.G5kAPI.GetJobState(d.G5kJobID)
 	if err != nil {
 		return state.Error, err
 	}
@@ -238,66 +217,26 @@ func (d *Driver) GetState() (state.State, error) {
 
 // PreCreateCheck check parameters and submit the job to Grid5000
 func (d *Driver) PreCreateCheck() (err error) {
-	// get api client
-	client := d.getAPI()
+	// create API client
+	d.G5kAPI = api.NewApi(d.G5kUsername, d.G5kPassword, d.G5kSite)
 
-	// reading ssh public key file
-	pubkey, err := ioutil.ReadFile(d.G5kSSHPublicKeyPath)
-	if err != nil {
+	// submit new job reservation
+	if err := d.submitNewJobReservation(); err != nil {
 		return err
 	}
 
-	// skip job reservation if an ID is already set
-	if d.G5kJobID == 0 {
-		// creating a new job with 1 node
-		jobReq := api.JobRequest{
-			Resources:  fmt.Sprintf("nodes=1,walltime=%s", d.G5kWalltime),
-			Command:    "sleep 365d",
-			Properties: d.G5kResourceProperties,
-			Types:      []string{"deploy"},
-		}
-
-		// submit job
-		d.G5kJobID, err = client.SubmitJob(jobReq)
+	// check if a SSH key pair is set
+	if d.SSHKeyPair == nil {
+		// generate a new SSH key pair
+		d.SSHKeyPair, err = ssh.NewKeyPair()
 		if err != nil {
-			return err
+			return fmt.Errorf("Error when generating a new SSH key pair: %s", err.Error())
 		}
-	} else {
-		log.Infof("Skipping job reservation and using job ID '%v'", d.G5kJobID)
 	}
 
-	// wait job
-	if err = client.WaitUntilJobIsReady(d.G5kJobID); err != nil {
+	// submit new deployment
+	if err := d.submitNewDeployment(); err != nil {
 		return err
-	}
-
-	// skip deployment if provisionning only mode is used
-	if d.G5kHostToProvision == "" {
-		// get job informations
-		job, err := client.GetJob(d.G5kJobID)
-		if err != nil {
-			return err
-		}
-
-		// creating a new deployment request
-		deploymentReq := api.DeploymentRequest{
-			Nodes:       job.Nodes,
-			Environment: d.G5kImage,
-			Key:         string(pubkey),
-		}
-
-		// deploy environment
-		d.G5kDeploymentID, err = client.SubmitDeployment(deploymentReq)
-		if err != nil {
-			return err
-		}
-
-		// waiting deployment to finish (REQUIRED or you will interfere with kadeploy)
-		if err = client.WaitUntilDeploymentIsFinished(d.G5kDeploymentID); err != nil {
-			return err
-		}
-	} else {
-		log.Infof("Skipping host deployment and provisionning host '%s' only", d.G5kHostToProvision)
 	}
 
 	return nil
@@ -311,23 +250,16 @@ func (d *Driver) Create() (err error) {
 		d.BaseDriver.IPAddress = d.G5kHostToProvision
 	} else {
 		// get hostname from API
-		client := d.getAPI()
-		job, err := client.GetJob(d.G5kJobID)
+		job, err := d.G5kAPI.GetJob(d.G5kJobID)
 		if err != nil {
 			return err
 		}
 		d.BaseDriver.IPAddress = job.Nodes[0]
 	}
 
-	// Copy the SSH private key to the docker machine config folder
-	src, dst := d.G5kSSHPrivateKeyPath, d.GetSSHKeyPath()
-	if err = mcnutils.CopyFile(src, dst); err != nil {
-		return err
-	}
-
-	// change private key file mode or ssh will complain about it
-	if err := os.Chmod(dst, 0600); err != nil {
-		return err
+	// copy SSH key pair to machine directory
+	if err := d.SSHKeyPair.WriteToFile(d.GetSSHKeyPath(), d.GetSSHKeyPath()+".pub"); err != nil {
+		return fmt.Errorf("Error when copying SSH key pair to machine directory: %s", err.Error())
 	}
 
 	return nil
@@ -335,12 +267,11 @@ func (d *Driver) Create() (err error) {
 
 // Remove delete the resources reservation
 func (d *Driver) Remove() error {
-	log.Info("Killing job...")
+	log.Infof("Killing job... (id: '%d')", d.G5kJobID)
 
-	client := d.getAPI()
-	client.KillJob(d.G5kJobID)
+	// send kill job command to API
+	d.G5kAPI.KillJob(d.G5kJobID)
 
-	// We get an error if the job was already dead, which is not really an error
 	return nil
 }
 
